f35cc16105dbbf1c7f1c90bcc657b10856b069e8
eldon-chung <eldon.chung.yr@gmail.com>
2025-05-21 17:39:36 +0800

    Fix baseUrl issue

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "learn-compsci.github.io/mathematical-techniques",
+    baseUrl: "learn-compsci.github.io/programming-in-C",
     ignorePatterns: ["learn-compsci", "templates", "Excalidraw", ".obsidian"],
     defaultDateType: "created",
     theme: {
